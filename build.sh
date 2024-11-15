1bb035e4e0f826d8689ae1ca71ace6a352fd61ac
Alex Tselegidis <alextselegidis@gmail.com>
2024-11-15 11:01:29 +0100

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Dependencies
+
+composer install
+
+# Empty Storage
+
+find storage/app -type f ! -name '.gitignore' -exec rm -f {} \;
+find storage/logs -type f ! -name '.gitignore' -exec rm -f {} \;
+
+# Clear Cache
+
+php artisan cache:clear
+php artisan route:clear
+php artisan config:clear
+php artisan view:clear
+php artisan clear-compiled
+
+# Remove Various
+
+rm build.zip
+
+rm public/hot
+
+find . -name ".DS_Store" -delete
+
+# Zip Files
+
+zip -r build.zip . \
+    -x '.git/*' \
+    -x '.idea/*' \
+    -x '.run/*' \
+    -x 'docker/*' \
+    -x 'node_modules/*' \
+    -x 'tests/*' \
+    -x '.editorconfig' \
+    -x '.gitattributes' \
+    -x '.gitignore' \
+    -x '.prettierignore' \
+    -x '.package-lock.json' \
+    -x '.env' \
+    -x 'build.sh' \
+    -x 'docker-compose.yml' \
+    -x 'postcss.config.js' \
+    -x 'tailwind.config.js' \
+    -x 'vite.config.js' \
+    -x '*.zip'
