01459499413958c308dd91450ece72241d00e426
Alex Tselegidis <alextselegidis@gmail.com>
2026-05-08 12:39:22 +0200

    Add custom session guard with unique recaller cookie name

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use App\Auth\AppSessionGuard;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Auth::extend('session', function ($app, $name, array $config) {
+            $provider = Auth::createUserProvider($config['provider'] ?? null);
+
+            $guard = new AppSessionGuard(
+                $name,
+                $provider,
+                $app['session.store'],
+                $app['request']
+            );
+
+            if (method_exists($guard, 'setCookieJar')) {
+                $guard->setCookieJar($app['cookie']);
+            }
+
+            if (method_exists($guard, 'setDispatcher')) {
+                $guard->setDispatcher($app['events']);
+            }
+
+            if (method_exists($guard, 'setRequest')) {
+                $guard->setRequest($app->refresh('request', $guard, 'setRequest'));
+            }
+
+            return $guard;
+        });
+
     }
 }
